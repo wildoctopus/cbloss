dd33c9ee44e7c5750605d9fb88bca57825d44efe
Alok Pandey <akphacker@live.com>
2023-05-11 12:55:15 +0200

    Added workflows

--- setup.py
+++ setup.py
@@ -1,25 +1,58 @@
 from setuptools import setup, find_packages
+import io
+import os
+import re
+
+
+
+def get_long_description():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    with io.open(os.path.join(base_dir, "README.md"), encoding="utf-8") as f:
+        return f.read()
+
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def get_version():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    version_file = os.path.join(current_dir, "balanced_loss", "__init__.py")
+    with io.open(version_file, encoding="utf-8") as f:
+        return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
+
+
+_DEV_REQUIREMENTS = [
+    "black==21.7b0",
+    "flake8==3.9.2",
+    "isort==5.9.2",
+    "click==8.0.4",
+    "importlib-metadata>=1.1.0,<4.3;python_version<'3.8'",
+]
+
+extras = {"tests": _DEV_REQUIREMENTS, "dev": _DEV_REQUIREMENTS}
 
 setup(
     name='cbloss',
-    version='0.1.0',
-    packages=find_packages(),
+    version=get_version(),
+    packages=find_packages(exclude=["tests"]),
     license='MIT',
     description='PyTorch implementations of two popular loss functions for imbalanced classification problems: Class Balanced Loss and Focal Loss.',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     author='Alok Pandey',
     url='https://github.com/wildoctopus/cbloss',
     keywords=['pytorch', 'loss', 'class-balanced', 'classification-loss', 'focal-loss', 'FocalLoss', 'cb_loss', 'cbloss'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    install_requires=[
-        'torch>=1.12.0',
-        'numpy>=1.19.2'
-    ],
+    install_requires=get_requirements(),
 )
